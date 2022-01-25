2a74daa70294255044279dc2e1b1ecb1578d2470
Niko <nikob381@gmail.com>
2022-01-25 12:20:09 -0500

    Completed sixth exercise

---
+++ make-it-modular.js
@@ -0,0 +1,7 @@
+const mymodule = require('./mymodule');
+
+mymodule(process.argv[2], process.argv[3], (err, list) => {
+  if (err) return console.log(err);
+
+  list.forEach((file) => console.log(file));
+});
