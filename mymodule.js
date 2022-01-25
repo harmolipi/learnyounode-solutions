53047ce7f9daa9bd9c10c2d501e37354fadb46fd
Niko <nikob381@gmail.com>
2022-01-25 12:19:54 -0500

    Created module for filtering and listing files

---
+++ mymodule.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+module.exports = (dir, ext, callback) => {
+  fs.readdir(dir, (err, list) => {
+    if (err) return callback(err);
+
+    list = list.filter((file) => file.endsWith(`.${ext}`));
+    callback(null, list);
+  });
+};
